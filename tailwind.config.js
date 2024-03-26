be025df0bc9c24a60504e9a49b4203b002315bc5
M Query <135724390+Meqr1@users.noreply.github.com>
2024-03-26 15:18:45 +0530

    init tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,ts,tsx,jsx}", "./components//**/*.{js,ts,tsx,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
