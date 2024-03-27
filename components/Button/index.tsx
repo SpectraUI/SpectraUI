cd31e2c52f469073e164af57534eb03a8b6562a7
M Query <135724390+Meqr1@users.noreply.github.com>
2024-03-27 15:12:51 +0530

    added Button Element

---
+++ components/Button/index.tsx
@@ -0,0 +1,132 @@
+import { ButtonHTMLAttributes, FunctionComponent } from "react";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  color?:
+    | "primary"
+    | "secondary"
+    | "green"
+    | "red"
+    | "yellow"
+    | "zinc"
+    | "cyan";
+  variant?: "outline" | "solid" | "flat" | "shadow";
+  size?: "lg" | "xl" | "sm" | "xs";
+  animated?: boolean;
+}
+
+const rendererVariant = (variant: string | undefined, color: string) => {
+  switch (variant) {
+    case "solid":
+      if (color !== "#71717A") {
+        return `bg-[${color}] text-gray-900`;
+      }
+      return `bg-[${color}] text-gray-100`;
+
+    case "outline":
+      return `border-2 border-[${color}] text-[${color}]`;
+
+    case "flat":
+      return `bg-[${color}] bg-opacity-30 text-[${color}]`;
+
+    case "shadow":
+      if (color !== "#71717A") {
+        return `bg-[${color}] text-gray-900 shadow-[0_8px_20px_-7px_${color}]`;
+      }
+      return `bg-[${color}] text-gray-100 shadow-[0_8px_20px_-7px_${color}]`;
+
+    default:
+      if (color !== "#71717A") {
+        return `bg-[${color}] text-gray-900`;
+      }
+      return `bg-[${color}] text-gray-100`;
+  }
+};
+
+export const Button: FunctionComponent<ButtonProps> = (props) => {
+  const {
+    className,
+    animated,
+    children,
+    color,
+    variant,
+    disabled,
+    ...otherProps
+  } = props;
+  let animated_: boolean;
+  if (animated === undefined) {
+    animated_ = true;
+  } else {
+    animated_ = animated;
+  }
+
+  let HexColor;
+  switch (color) {
+    case "secondary":
+      HexColor = "#7828C8";
+      break;
+    case "primary":
+      HexColor = "#3561FA";
+      break;
+    case "red":
+      HexColor = "#F31260";
+      break;
+    case "yellow":
+      HexColor = "#F5A524";
+      break;
+    case "green":
+      HexColor = "#17C964";
+      break;
+    case "zinc":
+      HexColor = "#71717A";
+      break;
+    case "cyan":
+      HexColor = "#7EE7FC";
+      break;
+    default:
+      HexColor = "#3561FA";
+      break;
+  }
+
+  // eslint-disable-next-line
+  let foo = [
+    "bg-[#71717A]",
+    "bg-[#7EE7FC]",
+    "bg-[#3561FA]",
+    "bg-[#7828C8]",
+    "bg-[#F31260]",
+    "bg-[#F5A524]",
+    "bg-[#17C964]",
+
+    "border-[#71717A]",
+    "border-[#7EE7FC]",
+    "border-[#3561FA]",
+    "border-[#7828C8]",
+    "border-[#F31260]",
+    "border-[#F5A524]",
+    "border-[#17C964]",
+
+    "shadow-[0_8px_20px_-7px_#71717A]",
+    "shadow-[0_8px_20px_-7px_#7EE7FC]",
+    "shadow-[0_8px_20px_-7px_#3561FA]",
+    "shadow-[0_8px_20px_-7px_#7828C8]",
+    "shadow-[0_8px_20px_-7px_#F31260]",
+    "shadow-[0_8px_20px_-7px_#F5A524]",
+    "shadow-[0_8px_20px_-7px_#17C964]",
+
+    "text-[#71717A]",
+    "text-[#7EE7FC]",
+    "text-[#3561FA]",
+    "text-[#7828C8]",
+    "text-[#F31260]",
+    "text-[#F5A524]",
+    "text-[#17C964]",
+  ];
+
+  const class_ = `px-2 text-sm py-1 m-2 rounded-lg ${animated_ ? "transition-all" : ""} ${className} ${rendererVariant(variant, HexColor)} ${disabled ? "opacity-50" : "opacity-100 active:scale-95"}`;
+
+  return (
+    <button className={class_} {...otherProps} disabled={disabled}>
+      {children}
+    </button>
+  );
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,99 @@
+import { Button } from "../components";
+
 function App() {
-  return <></>;
+  return (
+    <div className="bg-black">
+      <Button>Click</Button>
+      <Button color="cyan">Cyan</Button>
+      <Button color="yellow">Yellow</Button>
+      <Button color="secondary">Purple</Button>
+      <Button color="green">Green</Button>
+      <Button color="zinc">Gray</Button>
+      <Button color="red">Red</Button>
+      <br />
+      <Button variant="outline">Click</Button>
+      <Button color="cyan" variant="outline">
+        Cyan
+      </Button>
+      <Button color="yellow" variant="outline">
+        Yellow
+      </Button>
+      <Button color="secondary" variant="outline">
+        Purple
+      </Button>
+      <Button color="green" variant="outline">
+        Green
+      </Button>
+      <Button color="zinc" variant="outline">
+        Gray
+      </Button>
+      <Button color="red" variant="outline">
+        Red
+      </Button>
+      <br />
+      <Button variant="shadow">Click</Button>
+      <Button color="cyan" variant="shadow">
+        Cyan
+      </Button>
+      <Button color="yellow" variant="shadow">
+        Yellow
+      </Button>
+      <Button color="secondary" variant="shadow">
+        Purple
+      </Button>
+      <Button color="green" variant="shadow">
+        Green
+      </Button>
+      <Button color="zinc" variant="shadow">
+        Gray
+      </Button>
+      <Button color="red" variant="shadow">
+        Red
+      </Button>
+      <br />
+      <Button variant="flat">Click</Button>
+      <Button color="cyan" variant="flat">
+        Cyan
+      </Button>
+      <Button color="yellow" variant="flat">
+        Yellow
+      </Button>
+      <Button color="secondary" variant="flat">
+        Purple
+      </Button>
+      <Button color="green" variant="flat">
+        Green
+      </Button>
+      <Button color="zinc" variant="flat">
+        Gray
+      </Button>
+      <Button color="red" variant="flat">
+        Red
+      </Button>
+      <br />
+      <Button variant="flat" disabled>
+        Click
+      </Button>
+      <Button color="cyan" disabled>
+        Cyan
+      </Button>
+      <Button color="yellow" disabled>
+        Yellow
+      </Button>
+      <Button color="secondary" disabled>
+        Purple
+      </Button>
+      <Button color="green" disabled>
+        Green
+      </Button>
+      <Button color="zinc" disabled>
+        Gray
+      </Button>
+      <Button color="red" disabled>
+        Red
+      </Button>
+    </div>
+  );
 }
 
 export default App;
